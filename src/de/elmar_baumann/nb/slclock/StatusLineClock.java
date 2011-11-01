b84bb97fede00dae9c0879a603d8b3657e26dff0
ebaumann <ebaumann@localhost>
2011-11-01 22:18:51 +0100

    Using ScheduledExecutorService

--- src/de/elmar_baumann/nb/slclock/StatusLineClock.java
+++ src/de/elmar_baumann/nb/slclock/StatusLineClock.java
@@ -1,79 +1,81 @@
 package de.elmar_baumann.nb.slclock;
 
-import org.openide.awt.StatusLineElementProvider;
-
 import java.awt.BorderLayout;
 import java.awt.Component;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import java.text.DateFormat;
 import java.text.MessageFormat;
 import java.text.SimpleDateFormat;
-
 import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
 import javax.swing.SwingConstants;
-import javax.swing.Timer;
+
+import org.openide.awt.StatusLineElementProvider;
 
 /**
- * Displays date and time in the IDE's status line.
- *
- * The refresh interval is 1 second, the date's and time's display format is
- * DateFormat.SHORT.
+ * Displays date and time in the status line.
  *
  * @author  Elmar Baumann <eb@elmar-baumann.de>
  * @version 2009-04-02
  */
 public final class StatusLineClock implements StatusLineElementProvider {
-    public static final String      FORMAT_PATTERN;
-    private static final DateFormat WEEKDAY;
-    private static final DateFormat DATE_FORMAT;
-    private static final DateFormat TIME_FORMAT;
-    private static final int        REFRESH_INTERVAL_MILLI_SEC;
-    private final JLabel            label;
-    private final JPanel            panel;
 
-    static {
-        FORMAT_PATTERN             = "  {0} {1} - {2}";
-        REFRESH_INTERVAL_MILLI_SEC = 1000;
-        WEEKDAY                    = new SimpleDateFormat("E");
-        DATE_FORMAT = DateFormat.getDateInstance(DateFormat.SHORT);
-        TIME_FORMAT = DateFormat.getTimeInstance(DateFormat.SHORT);
-    }
+    private static final String FORMAT_PATTERN = "  {0} {1} - {2}";
+    private static final DateFormat WEEKDAY_FORMAT = new SimpleDateFormat("E");
+    private static final DateFormat DATE_FORMAT = DateFormat.getDateInstance(DateFormat.SHORT);
+    private static final DateFormat TIME_FORMAT = DateFormat.getTimeInstance(DateFormat.SHORT);
+    private static final int REFRESH_INTERVAL_MILLISECONDS = 1000;
+    private final JLabel clockLabel = new JLabel(getCurrentDateTimeString());
+    private final JPanel statusLinePanel = new JPanel(new BorderLayout());
+    private final ScheduledExecutorService scheduler;
 
     public StatusLineClock() {
-        label = new JLabel(currentDateTimeText());
-        panel = new JPanel(new BorderLayout());
-        init();
+        scheduler = Executors.newSingleThreadScheduledExecutor(threadFactory);
+        initComponents();
+        scheduler.scheduleWithFixedDelay(clockLabelUpdater, 0, REFRESH_INTERVAL_MILLISECONDS, TimeUnit.MILLISECONDS);
     }
 
+    private void initComponents() {
+        statusLinePanel.add(new JSeparator(SwingConstants.VERTICAL), BorderLayout.WEST);
+        statusLinePanel.add(clockLabel, BorderLayout.CENTER);
+    }
+
+    @Override
     public Component getStatusLineElement() {
-        return panel;
+        return statusLinePanel;
     }
 
-    private void init() {
-        Timer timer = new Timer(REFRESH_INTERVAL_MILLI_SEC, new DatePainter());
-
-        timer.start();
-        panel.add(new JSeparator(SwingConstants.VERTICAL), BorderLayout.WEST);
-        panel.add(label, BorderLayout.CENTER);
-    }
-
-    private static String currentDateTimeText() {
+    private static String getCurrentDateTimeString() {
         Date now = new Date();
 
-        return MessageFormat.format(FORMAT_PATTERN, WEEKDAY.format(now),
-                                    DATE_FORMAT.format(now),
-                                    TIME_FORMAT.format(now));
+        return MessageFormat.format(FORMAT_PATTERN,
+                WEEKDAY_FORMAT.format(now),
+                DATE_FORMAT.format(now),
+                TIME_FORMAT.format(now));
     }
 
-    private class DatePainter implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            label.setText(currentDateTimeText());
+    private final Runnable clockLabelUpdater = new Runnable() {
+
+        public void run() {
+            clockLabel.setText(getCurrentDateTimeString());
         }
-    }
+    };
+
+    private final ThreadFactory threadFactory = new ThreadFactory() {
+
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread thread = new Thread(r);
+
+            thread.setName("StatusLineClock: Displaying Date and Time in the Status Line");
+
+            return thread;
+        }
+    };
 }
